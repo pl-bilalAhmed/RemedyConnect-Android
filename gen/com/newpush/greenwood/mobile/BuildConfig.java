1f9bebad3aa284c0ae6a74f6aedbe6bafc6f67d6
Zoltán Adamek <adamek.zoltan@gmail.com>
2012-09-22 23:56:02 +0200

    Modified app settings.

--- gen/com/newpush/greenwood/mobile/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.newpush.greenwood.mobile;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
---
+++ gen/com/newpush/greenwoodpediatrics/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.newpush.greenwoodpediatrics;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
