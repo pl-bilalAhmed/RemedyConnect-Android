9971c4aa335492a3ce69b432a2da31fe9080552a
Zoltán Adamek <adamek.zoltan@gmail.com>
2012-09-25 18:16:16 +0200

    Added new parser to PracticeNewsActivity.

--- src/com/newpush/greenwoodpediatrics/PracticeNewsActivity.java
+++ src/com/newpush/greenwoodpediatrics/PracticeNewsActivity.java
@@ -1,12 +1,6 @@
 package com.newpush.greenwoodpediatrics;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -22,6 +16,8 @@ import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.support.v4.app.NavUtils;
 
+import com.newpush.greenwoodpediatrics.Parser;
+
 public class PracticeNewsActivity extends Activity {
 	protected ArrayAdapter<String> newsAdapter;
 	protected ListView newsList;
@@ -30,13 +26,13 @@ public class PracticeNewsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice_news);
-        
+
         newsList = (ListView) findViewById(R.id.practiceNewsListView);
         newsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
         newsList.setAdapter(newsAdapter);
         new ParseNews().execute();
     }
-    
+
     protected void attachNewsItemLinks() {
     	newsList.setOnItemClickListener(new OnItemClickListener() {
     		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -49,29 +45,18 @@ public class PracticeNewsActivity extends Activity {
 
     private class ParseNews extends AsyncTask<Void, Void, String[]> {
     	protected String[] doInBackground(Void... params) {
-    		ArrayList<String> titles = new ArrayList<String>();
-    		String filename = getApplicationContext().getFilesDir().getAbsolutePath() + "/news.xml";
-    		File newsXML = new File(filename);
-			Document doc;
-			try {
-				doc = Jsoup.parse(newsXML, "UTF-8", "");
-				for (Element titleelement : doc.select("NewsTitle")) {
-					titles.add(titleelement.text());
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+            Parser parser = new Parser(getApplicationContext().getFilesDir().getAbsolutePath() + "/news.xml");
+            ArrayList<String> titles = parser.Parse("NewsTitle");
     		String[] result = new String[titles.size()];
 			titles.toArray(result);
 			return result;
     	}
-    	
+
     	protected void onPostExecute(String[] result) {
     		for (String s : result) {
     			newsAdapter.add(s);
     		}
     		attachNewsItemLinks();
-    	} 	
+    	}
     }
 }
