7f01934c9c25a7a0a3ac09bd40549ba090dcef80
Zoltán Adamek <adamek.zoltan@gmail.com>
2012-09-28 19:28:19 +0200

    Refactores office item parser logic.

--- src/com/newpush/greenwoodpediatrics/OfficeInfoItemActivity.java
+++ src/com/newpush/greenwoodpediatrics/OfficeInfoItemActivity.java
@@ -1,6 +1,7 @@
 package com.newpush.greenwoodpediatrics;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,36 +12,35 @@ import android.view.MenuItem;
 import android.webkit.WebView;
 import android.support.v4.app.NavUtils;
 
-import com.newpush.greenwoodpediatrics.parser.DefaultParser;
+import com.newpush.greenwoodpediatrics.parser.OfficeInfoParser;
 
 public class OfficeInfoItemActivity extends Activity {
 	Bundle extras;
 	WebView display;
 
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-				super.onCreate(savedInstanceState);
-				setContentView(R.layout.activity_office_info_item);
-				display = (WebView) findViewById(R.id.contentWebView);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.activity_office_info_item);
+			display = (WebView) findViewById(R.id.contentWebView);
 
-				extras = getIntent().getExtras();
-				new ParseOfficeInfoItem().execute(extras.getInt("which"));
+			extras = getIntent().getExtras();
+			new ParseOfficeInfoItem().execute(extras.getInt("which"));
+	}
+
+	private class ParseOfficeInfoItem extends AsyncTask<Integer, Void, Hashtable<String, String>> {
+		protected Hashtable<String, String> doInBackground(Integer... params) {
+			OfficeInfoParser parser = new OfficeInfoParser(getApplicationContext());
+			Hashtable<String, String> result = parser.getFullInfo(params[0]);
+			return result;
 		}
 
-		private class ParseOfficeInfoItem extends AsyncTask<Integer, Void, String[]> {
-			protected String[] doInBackground(Integer... params) {
-				String index = params[0].toString();
-				DefaultParser parser = new DefaultParser(getApplicationContext().getFilesDir().getAbsolutePath() + "/office.xml");
-				String resultTitle = parser.ParseSingle("pw_Message:eq(" + index + ") messageTitle");
-				String resultHTML = parser.ParseSingle("pw_Message:eq(" + index + ") StandardMessage");
-				String[] result = {resultTitle, resultHTML};
-				return result;
-			}
-
-			protected void onPostExecute(String[] result) {
-				setTitle(result[0]);
-				display.loadData(result[1], "text/html", "utf-8");
-			}
-
+		protected void onPostExecute(Hashtable<String, String> result) {
+			String title = (String)result.get("title");
+			String message = OfficeInfoParser.formatTitle(title) + (String)result.get("message");
+			setTitle(title);
+			display.loadData(message, "text/html", "utf-8");
 		}
+
+	}
 }
