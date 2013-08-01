c165a24f7250493e08f426d22a81312275dbf19d
Zoltán Adamek <zadamek@newpush.com>
2013-08-01 13:02:49 +0200

    Adds zip extraction for design packs

--- src/com/newpush/mypractice/Skin.java
+++ src/com/newpush/mypractice/Skin.java
@@ -5,29 +5,32 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.widget.ImageView;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
 
 import java.io.File;
 
 public class Skin {
-    public static String getSkinDirectoryPath() {
-        /*
-        @TODO Use a fool-proof location for storing and using skins
-         Right now this is only for testing which works on my SGS, but it
-         won't work on anything else - most likely. For accessing the files
-         in the application's data storage, use:
-
-         String skinDir = context.getFilesDir().getAbsolutePath() + "/skin/";
-         */
-        return "/storage/sdcard0/"; //pediatricweb/skin/";
+    public static String getSkinDirectoryPath(Context context) {
+        return context.getFilesDir().getAbsolutePath() + "/skin/";
     }
 
+    public static void ExtractDesignPack(Context context) {
+        try {
+            ZipFile designPack = new ZipFile(getSkinDirectoryPath(context) + "DesignPack.zip");
+            designPack.extractAll(getSkinDirectoryPath(context));
+        } catch (ZipException e) {
+            e.printStackTrace();
+        }
+   }
+
     public static void prepareSkinDirectory(Context context) {
-        File skinFolder = new File(getSkinDirectoryPath());
+        File skinFolder = new File(getSkinDirectoryPath(context));
         skinFolder.mkdir();
     }
 
     public static void applyThemeSplash(Activity splashActivity) {
-        File splashFile = new File(getSkinDirectoryPath() + "mountain.png");
+        File splashFile = new File(getSkinDirectoryPath(splashActivity) + "splashscreen.png");
         if (splashFile.exists()) {
             Bitmap splashBitmap = BitmapFactory.decodeFile(splashFile.getAbsolutePath());
             ImageView splashView = (ImageView) splashActivity.findViewById(R.id.splash_image);
@@ -36,7 +39,7 @@ public class Skin {
     }
     
     public static void applyThemeLogo(Activity activityWithLogo) {
-        File logoFile = new File(getSkinDirectoryPath() + "mountain.png");
+        File logoFile = new File(getSkinDirectoryPath(activityWithLogo) + "logo.png");
         if (logoFile.exists()) {
             Bitmap logoBitmap = BitmapFactory.decodeFile(logoFile.getAbsolutePath());
             ImageView logoView = (ImageView) activityWithLogo.findViewById(R.id.logo);
