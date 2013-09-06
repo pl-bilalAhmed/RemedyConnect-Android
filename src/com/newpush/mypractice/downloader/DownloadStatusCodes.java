fe9ff541ffa7885f859648469751b1e16e26ad02
Zoltán Adamek <zadamek@newpush.com>
2013-09-06 18:50:24 +0200

    Moving downloader code
    Nim has requested to modify the download UX: we should fire up the download from the practice selection screen or from the update practice info menu item directly without any unnecessary screen.

--- src/com/newpush/mypractice/downloader/DownloadStatusCodes.java
+++ src/com/newpush/mypractice/downloader/DownloadStatusCodes.java
@@ -1,12 +1,12 @@
 package com.newpush.mypractice.downloader;
 
-public class DownloadStatus {
+public class DownloadStatusCodes {
     public static final int UPDATE_PROGRESS = 1;
     public static final int DOWNLOAD_FAILED = 2;
     public static final int NETWORK_AVAILABLE = 3;
     public static final int SWITCH_TO_DETERMINATE = 4;
     public static final int DOWNLOAD_FINISHED = 5;
-    public static final int SWITCH_TO_NON_DETERMINATE = 6;
+    public static final int PARSING_FOR_MORE = 6;
     public static final int EXTRACTING = 7;
     public static final int EXTRACTING_FAILED = 8;
     public static final int EXTRACTING_FINISHED = 9;
