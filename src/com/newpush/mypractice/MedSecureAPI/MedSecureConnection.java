400f24c7ce8767807ad985dc7993e66cebe79783
Zoltán Adamek <zadamek@newpush.com>
2013-11-26 18:38:30 +0100

    First testing code version which actually works for pulling a sample practice and displaying it as a toast.

--- src/com/newpush/mypractice/MedSecureAPI/MedSecureConnection.java
+++ src/com/newpush/mypractice/MedSecureAPI/MedSecureConnection.java
@@ -2,17 +2,27 @@ package com.newpush.mypractice.MedSecureAPI;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.*;
 
 public class MedSecureConnection  {
+    // Configuration
+    static final String base = "https://MedSecureAPI.com/";
+    static final String auth = "Basic UmFscGg6Q29yaWdsaWFubw==";
+    static final String api_key = "SSB3aWxsIG1ha2UgbXkgQVBJIHNlY3VyZQ%3d%3d";
+    static final String token = "j2w%2bjHHs%2bF8fkvr7Vj5DlPuYg8VqXvOhbtaG4WaOqxA%3d";
+    static final String api_token_post = "apikey=" + api_key + "&token=" + token;
+    static final String charset = "UTF-8";
+    static final int readTimeout = 10000;
+    static final int connectTimeout = 15000;
+
+    // Fields
     private Context context;
 
+    // Setters / getters
     public Context getContext() {
         return context;
     }
@@ -21,19 +31,16 @@ public class MedSecureConnection  {
         this.context = context;
     }
 
-    public static final String base = "https://MedSecureAPI.com/";
+    // Public interface
 
-    static final String auth = "Basic UmFscGg6Q29yaWdsaWFubw==";
-    static final String api_key = "SSB3aWxsIG1ha2UgbXkgQVBJIHNlY3VyZQ%3d%3d";
-    static final String token = "j2w%2bjHHs%2bF8fkvr7Vj5DlPuYg8VqXvOhbtaG4WaOqxA%3d";
-    static final String api_token_post = "apikey=&" + api_key + "&token=" + token;
-
+    // Starts sample async API task
     public void startAsyncGetPractice(Integer practiceID) {
-        AsyncAPITask task = new AsyncAPITask();
+        SampleAsyncAPITask task = new SampleAsyncAPITask();
         task.execute(new String[] { practiceID.toString() });
     }
 
-    public class AsyncAPITask extends AsyncTask<String, Void, String> {
+    // Sample async API task
+    public class SampleAsyncAPITask extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... params) {
             return getPractice(Integer.parseInt(params[0]));
@@ -45,30 +52,52 @@ public class MedSecureConnection  {
         }
     }
 
+    // Method for fetching practice info
     public String getPractice(int practiceID) {
         try {
-            URL url = new URL(base + "api/Practice/GetPractice?practiceID=" + practiceID + "&" + api_token_post);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestProperty("Authorization", auth);
-            connection.setRequestProperty("Content-Type", "application/json");
-            InputStream stream = null;
-            connection.connect();
-
-            StringBuffer output = new StringBuffer("");
-            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                stream = connection.getInputStream();
-                BufferedReader buffer = new BufferedReader(new InputStreamReader(stream));
-                String s = "";
-                while ((s = buffer.readLine()) != null) {
-                    output.append(s);
-                }
-            }
-            return output.toString();
+            return httpGetURL(base + "api/Practice/GetPractice?practiceID=" + practiceID + "&" + api_token_post);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            e.printStackTrace();
         }
         return "";
+    }
+
+    // Utilities
+    private String readStreamToString(InputStream is) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is, charset));
+        StringBuilder out = new StringBuilder();
+        String line;
+        while((line = reader.readLine()) != null) {
+            out.append(line);
+        }
+        return out.toString();
+    }
+
+    private String httpGetURL(String URLToDownload) throws IOException {
+        InputStream is = null;
+
+        try {
+            URL url = new URL(URLToDownload);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(readTimeout);
+            connection.setConnectTimeout(connectTimeout);
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.setRequestProperty("Authorization", auth);
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.connect();
+
+            is = connection.getInputStream();
+
+            String content = readStreamToString(is);
+            return content;
+        }
+        finally {
+            if (is != null) {
+                is.close();
+            }
+        }
     }
 }
