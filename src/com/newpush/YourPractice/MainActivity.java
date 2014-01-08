72ac5dfee982f1953e3fadb96f1c8255aba92e7c
Zoltán Adamek <zadamek@newpush.com>
2014-01-08 12:43:27 +0100

    Starts preparing ProGuard settings.

--- src/com/newpush/YourPractice/MainActivity.java
+++ src/com/newpush/YourPractice/MainActivity.java
@@ -3,23 +3,12 @@ package com.newpush.YourPractice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
-import com.pushio.manager.PushIOManager;
 
 public class MainActivity extends DefaultActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        PushIOManager mPushIOManager = PushIOManager.getInstance(this);
-        mPushIOManager.ensureRegistration();
-
-        //Checking to see if this activity was created by a user engaging with a notification
-        String PUSH_KEY_ALERT = "alert";
-        if (getIntent().hasExtra(PUSH_KEY_ALERT)) {
-            //Since they did engage with a notification we can grab anything from the payload, for this simple case, let's display the text from the last alert.
-            Toast.makeText(this, "Last Push: " + getIntent().getStringExtra(PUSH_KEY_ALERT), Toast.LENGTH_LONG).show();
-        }
     }
 
     public void checkDownloadedData() {
