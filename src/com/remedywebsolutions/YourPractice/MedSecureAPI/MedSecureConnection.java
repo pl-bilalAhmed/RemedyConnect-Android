be24a9415185eda6a0158728db06517f22f57e19
Zoltán Adamek <zadamek@newpush.com>
2014-02-16 23:33:49 +0100

    Cleans up connection module.

--- src/com/remedywebsolutions/YourPractice/MedSecureAPI/MedSecureConnection.java
+++ src/com/remedywebsolutions/YourPractice/MedSecureAPI/MedSecureConnection.java
@@ -1,21 +1,11 @@
 package com.remedywebsolutions.YourPractice.MedSecureAPI;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Build;
-import android.util.Base64;
-import android.widget.Toast;
-import com.pushio.manager.PushIOManager;
 import org.apache.commons.io.IOUtils;
 
 import java.io.*;
 import java.net.*;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
 
 /**
  * Class for handling connections with the MedSecure API.
@@ -28,12 +18,9 @@ public class MedSecureConnection  {
     static final String api_key = "SSB3aWxsIG1ha2UgbXkgQVBJIHNlY3VyZQ%3d%3d";
     static final String token = "j2w%2bjHHs%2bF8fkvr7Vj5DlPuYg8VqXvOhbtaG4WaOqxA%3d";
     static final String charset = "UTF-8";
-    static final String prefKey = "RemedyWebSolutionsYourPractice";
     static final int readTimeout = 10000;
     static final int connectTimeout = 15000;
 
-    // Fields
-    private Context context;
     private Uri.Builder uriBuilder;
     private String method;
 
@@ -106,44 +93,4 @@ public class MedSecureConnection  {
     public static String getStringResult(HttpURLConnection connection) throws IOException {
         return IOUtils.toString(connection.getInputStream(), charset);
     }
-
-
-    // Setters / getters
-    public Context getContext() {
-        return context;
-    }
-
-    public void setContext(Context context) {
-        this.context = context;
-    }
-
-    // Public interface
-
-    public static boolean isLoggedIn() {
-        return false;
-    }
-
-    // Storing data about users currently logged in ---------------------------------------------------------------------
-    public void fetchUserDataForStoring() {
-
-    }
-
-    public void StoreData() {
-        SharedPreferences sp = context.getSharedPreferences(prefKey, Activity.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putInt("physicianID", 0);
-        editor.putString("deviceID", "");
-        editor.putString("name", "");
-        editor.commit();
-    }
-
-    public HashMap<String, String> RetrieveData() {
-        SharedPreferences sp = context.getSharedPreferences(prefKey, Activity.MODE_PRIVATE);
-        int myIntValue = sp.getInt("your_int_key", -1);
-
-        HashMap<String, String> userData = new HashMap<String, String>(5);
-        return userData;
-    }
-
-
 }
