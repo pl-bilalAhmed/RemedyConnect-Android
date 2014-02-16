91398ade24f872943eb1a9c4147425b774e9e04a
Zoltán Adamek <zadamek@newpush.com>
2014-02-16 23:27:00 +0100

    Major refactoring: adds better API call building.

--- src/com/remedywebsolutions/YourPractice/MedSecureAPI/MedSecureConnection.java
+++ src/com/remedywebsolutions/YourPractice/MedSecureAPI/MedSecureConnection.java
@@ -3,10 +3,13 @@ package com.remedywebsolutions.YourPractice.MedSecureAPI;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.util.Base64;
 import android.widget.Toast;
 import com.pushio.manager.PushIOManager;
+import org.apache.commons.io.IOUtils;
 
 import java.io.*;
 import java.net.*;
@@ -14,13 +17,16 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 
+/**
+ * Class for handling connections with the MedSecure API.
+ *
+ * All implementation details (endpoints, connection settings etc.) should be here.
+ */
 public class MedSecureConnection  {
-    // Configuration
-    static final String base = "https://MedSecureAPI.com/";
+    static final String base = "https://MedSecureAPI.com/api/";
     static final String auth = "Basic em9sdGFuOnpvbHRhbjE=";
     static final String api_key = "SSB3aWxsIG1ha2UgbXkgQVBJIHNlY3VyZQ%3d%3d";
     static final String token = "j2w%2bjHHs%2bF8fkvr7Vj5DlPuYg8VqXvOhbtaG4WaOqxA%3d";
-    static final String api_token_post = "apikey=" + api_key + "&token=" + token;
     static final String charset = "UTF-8";
     static final String prefKey = "RemedyWebSolutionsYourPractice";
     static final int readTimeout = 10000;
@@ -28,6 +34,78 @@ public class MedSecureConnection  {
 
     // Fields
     private Context context;
+    private Uri.Builder uriBuilder;
+    private String method;
+
+    /**
+     * Constructor.
+     */
+    public MedSecureConnection() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.FROYO) {
+            System.setProperty("http.keepAlive", "false");
+        }
+        uriBuilder = Uri.parse(base).buildUpon();
+    }
+
+    /**
+     * Builds up base URI for the call.
+     *
+     * @param controllerName The name of the controller in the API.
+     * @param functionName The name of the function in the API.
+     * @param method The HTTP method for the function.
+     */
+    public void buildBaseURI(String controllerName, String functionName, String method) {
+        uriBuilder.appendPath(controllerName);
+        uriBuilder.appendPath(functionName);
+        this.method = method;
+    }
+
+    /**
+     * Adds a parameter to the call.
+     *
+     * @param parameterName The key of the parameter.
+     * @param parameterValue The value of the parameter.
+     */
+    public void addParameter(String parameterName, String parameterValue) {
+        uriBuilder.appendQueryParameter(parameterName, parameterValue);
+    }
+
+    /**
+     * Adds default API parameters to the call.
+     */
+    private void addAPIParameters() {
+        uriBuilder.appendQueryParameter("apikey", api_key);
+        uriBuilder.appendQueryParameter("token", token);
+    }
+
+    /**
+     * Initializes a HTTP connection with the default settings.
+     *
+     * @return The initialized HTTP connection.
+     * @throws IOException
+     */
+    public HttpURLConnection initConnection() throws IOException {
+        addAPIParameters();
+        HttpURLConnection connection = (HttpURLConnection) new URL(uriBuilder.build().toString()).openConnection();
+        connection.setReadTimeout(readTimeout);
+        connection.setConnectTimeout(connectTimeout);
+        connection.setRequestMethod(method);
+        connection.setDoInput(true);
+        connection.setRequestProperty("Authorization", auth);
+        connection.setRequestProperty("Content-Type", "application/json");
+        return connection;
+    }
+
+    /**
+     * Returns the entire response as a string.
+     *
+     * @param connection The connection to get the result.
+     * @return The result.
+     * @throws IOException
+     */
+    public static String getStringResult(HttpURLConnection connection) throws IOException {
+        return IOUtils.toString(connection.getInputStream(), charset);
+    }
 
     // Setters / getters
     public Context getContext() {
@@ -86,4 +164,6 @@ public class MedSecureConnection  {
         }
         return result;
     }
+
+
 }
--- src/com/remedywebsolutions/YourPractice/MedSecureAPI/requests/RegisterDeviceRequest.java
+++ src/com/remedywebsolutions/YourPractice/MedSecureAPI/requests/RegisterDeviceRequest.java
@@ -1,25 +1,11 @@
 package com.remedywebsolutions.YourPractice.MedSecureAPI.requests;
 
-import android.os.Build;
 import com.octo.android.robospice.request.SpiceRequest;
-import com.pushio.manager.PushIOManager;
 import com.remedywebsolutions.YourPractice.MedSecureAPI.MedSecureConnection;
-import org.apache.commons.io.IOUtils;
 
 import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class RegisterDeviceRequest extends SpiceRequest<String> {
-    // @TODO This is now just a plain copy of the info in the MedSecureConnection.java, let's clean this up later
-    static final String base = "https://MedSecureAPI.com/";
-    static final String auth = "Basic em9sdGFuOnpvbHRhbjE=";
-    static final String api_key = "SSB3aWxsIG1ha2UgbXkgQVBJIHNlY3VyZQ%3d%3d";
-    static final String token = "j2w%2bjHHs%2bF8fkvr7Vj5DlPuYg8VqXvOhbtaG4WaOqxA%3d";
-    static final String api_token_post = "apikey=" + api_key + "&token=" + token;
-    static final String charset = "UTF-8";
-    static final int readTimeout = 10000;
-    static final int connectTimeout = 15000;
-
     private String userId;
     private String username;
 
@@ -31,40 +17,14 @@ public class RegisterDeviceRequest extends SpiceRequest<String> {
 
     @Override
     public String loadDataFromNetwork() throws Exception {
-        // We should use this technique after the first testing:
-        // With Uri.Builder class we can build our url is a safe manner
-        /*
-        Uri.Builder uriBuilder = Uri.parse(
-                "http://robospice-sample.appspot.com/reverse").buildUpon();
-        uriBuilder.appendQueryParameter("word", "my name is");
-
-        String url = uriBuilder.build().toString();
-        */
-
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.FROYO) {
-            System.setProperty("http.keepAlive", "false");
-        }
-
-        MedSecureConnection ms = new MedSecureConnection();
-        String deviceId = ms.getPushIOHash(username);
-
-        String urlToDownload = base +
-                "api/Physician/InsertPhysicianMobileDevice?physicianID=" + userId + "&" +
-                "deviceID=" + deviceId + "&" +
-                api_token_post;
-        String method = "GET";
-
-        HttpURLConnection urlConnection = (HttpURLConnection) new URL(urlToDownload)
-                .openConnection();
-        urlConnection.setReadTimeout(readTimeout);
-        urlConnection.setConnectTimeout(connectTimeout);
-        urlConnection.setRequestMethod(method);
-        urlConnection.setDoInput(true);
-        urlConnection.setRequestProperty("Authorization", auth);
-        urlConnection.setRequestProperty("Content-Type", "application/json");
-
-        String result = IOUtils.toString(urlConnection.getInputStream(), charset);
-        urlConnection.disconnect();
+        MedSecureConnection msc = new MedSecureConnection();
+        String deviceId = msc.getPushIOHash(username);
+        msc.buildBaseURI("Physician", "InsertPhysicianMobileDevice", "GET");
+        msc.addParameter("physicianID", userId);
+        msc.addParameter("deviceID", deviceId);
+        HttpURLConnection connection = msc.initConnection();
+        String result = MedSecureConnection.getStringResult(connection);
+        connection.disconnect();
 
         return deviceId;
     }
