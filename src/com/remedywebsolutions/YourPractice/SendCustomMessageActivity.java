901f64d70091d42a2404be264f1ccb2020177a09
Zoltán Adamek <zadamek@newpush.com>
2014-03-13 16:24:46 +0100

    Implements fetching contacts for sending new messages.

--- src/com/remedywebsolutions/YourPractice/SendCustomMessageActivity.java
+++ src/com/remedywebsolutions/YourPractice/SendCustomMessageActivity.java
@@ -7,14 +7,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.octo.android.robospice.SpiceManager;
 import com.octo.android.robospice.UncachedSpiceService;
 import com.octo.android.robospice.persistence.exception.SpiceException;
 import com.octo.android.robospice.request.listener.RequestListener;
 import com.remedywebsolutions.YourPractice.MedSecureAPI.LoggedInDataStorage;
 import com.remedywebsolutions.YourPractice.MedSecureAPI.POJOs.InAppNotificationRequestContent;
+import com.remedywebsolutions.YourPractice.MedSecureAPI.POJOs.Physician;
+import com.remedywebsolutions.YourPractice.MedSecureAPI.POJOs.PhysiciansResponse;
 import com.remedywebsolutions.YourPractice.MedSecureAPI.requests.SendInAppNotificationRequest;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
@@ -22,13 +26,14 @@ import java.util.UUID;
 public class SendCustomMessageActivity extends DefaultActivity {
     private SpiceManager spiceManager = new SpiceManager(UncachedSpiceService.class);
 
-    EditText subjectEditText, messageEditText;
-    Spinner recipientSpinner;
+    private EditText subjectEditText, messageEditText;
+    private Spinner recipientSpinner;
     private ArrayAdapter<String> recipientAdapter;
     private String subjectForReply;
     private int toPhysicianIDForReply;
     private String conversationIDForReply;
     private boolean replyMode;
+    private PhysiciansResponse physicians;
 
     @Override
     protected void onStart() {
@@ -50,6 +55,15 @@ public class SendCustomMessageActivity extends DefaultActivity {
         messageEditText = (EditText) findViewById(R.id.messageEditText);
         recipientSpinner = (Spinner) findViewById(R.id.recipientSpinner);
 
+        LoggedInDataStorage storage = new LoggedInDataStorage(this);
+        HashMap<String, String> userData = storage.RetrieveData();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            physicians = mapper.readValue(userData.get("physicians"), PhysiciansResponse.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         // Set up adapter for recipient
         recipientAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, new ArrayList<String>()) {
             @Override
@@ -65,8 +79,12 @@ public class SendCustomMessageActivity extends DefaultActivity {
             }
         };
         recipientSpinner.setAdapter(recipientAdapter);
-
-        subjectForReply = extras.getString("subject");
+        if (extras != null) {
+            subjectForReply = extras.getString("subject");
+        }
+        else {
+            subjectForReply = null;
+        }
         replyMode = (subjectForReply != null);
         if (replyMode) {
             recipientSpinner.setEnabled(false);
@@ -75,6 +93,11 @@ public class SendCustomMessageActivity extends DefaultActivity {
             subjectEditText.setText(subjectForReply);
             subjectEditText.setEnabled(false); // @TODO Should we leave this editable?
             recipientAdapter.add(extras.getString("toPhysicianName"));
+        }
+        else {
+            for (Physician physician : physicians.physicians) {
+                recipientAdapter.add(physician.physicianName);
+            }
         }
 
         progress = new ProgressDialog(this);
@@ -97,7 +120,9 @@ public class SendCustomMessageActivity extends DefaultActivity {
                     message.conversationID = conversationIDForReply;
                 }
                 else {
-                    message.toPhysicianID = 405; // @TODO Implement fetching from contact list
+                    // Get position in recipient spinner
+                    int position = recipientSpinner.getSelectedItemPosition();
+                    message.toPhysicianID = physicians.physicians.get(position).physicianID;
                     message.subject = subjectEditText.getText().toString();
                 }
                 message.message = messageEditText.getText().toString();
