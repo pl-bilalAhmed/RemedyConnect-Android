3250488e7f07a034e6c0e10c1fb3cac6108ebff8
Zoltán Adamek <zadamek@newpush.com>
2014-03-17 18:26:27 +0100

    Moves notification handling from main menu to My Account.

--- YourPractice/src/main/java/com/remedywebsolutions/YourPractice/MainActivity.java
+++ YourPractice/src/main/java/com/remedywebsolutions/YourPractice/MainActivity.java
@@ -2,7 +2,6 @@ package com.remedywebsolutions.YourPractice;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 import com.pushio.manager.PushIOManager;
 
 public class MainActivity extends DefaultActivity {
@@ -15,13 +14,6 @@ public class MainActivity extends DefaultActivity {
 
         mPushIOManager = PushIOManager.getInstance(this);
         mPushIOManager.ensureRegistration();
-
-        //Checking to see if this activity was created by a user engaging with a notification
-        if (getIntent().hasExtra(PUSH_KEY_ALERT)) {
-            // @TODO Handle incoming notifications while the app is running
-            // Since they did engage with a notification we can grab anything from the payload, for this simple case, let's display the text from the last alert.
-            // Toast.makeText(this, "Last Push: " + getIntent().getStringExtra(PUSH_KEY_ALERT), Toast.LENGTH_LONG).show();
-        }
     }
 
     public void checkDownloadedData() {
