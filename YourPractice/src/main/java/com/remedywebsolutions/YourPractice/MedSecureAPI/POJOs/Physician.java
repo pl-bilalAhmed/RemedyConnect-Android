2705ed20a842f264cf7e8cae7e72e7db8778b5ef
Zoltán Adamek <djscorchio@gmail.com>
2014-04-03 16:21:35 +0200

    Adds extended physician info.

--- YourPractice/src/main/java/com/remedywebsolutions/YourPractice/MedSecureAPI/POJOs/Physician.java
+++ YourPractice/src/main/java/com/remedywebsolutions/YourPractice/MedSecureAPI/POJOs/Physician.java
@@ -1,6 +1,8 @@
 package com.remedywebsolutions.YourPractice.MedSecureAPI.POJOs;
 
 public class Physician {
-    public int physicianID;
-    public String physicianName;
+    public int physicianID, practiceID, pagerCoID;
+    public String firstName, mi, lastName, physicianName, phoneticSpelling, title;
+    public String email, pager, home, pagingCompany, cell, fax, homeFax;
+    public String office, officeExt, daysWorked, lastUpdatedBy, lastUpdate;
 }
