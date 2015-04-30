e0453a4df22701da95eadf52f5acdc2f5aff30c2
Ken Sciacca <ksciacca@istechnology.net>
2015-04-30 11:39:54 -0600

    pin timeout from server

---
+++ YourPractice/src/main/java/com/remedywebsolutions/YourPractice/MedSecureAPI/POJOs/GetProviderPinTimeoutResponse.java
@@ -0,0 +1,12 @@
+package com.remedywebsolutions.YourPractice.MedSecureAPI.POJOs;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ksciacca on 3/20/2015.
+ */
+public class GetProviderPinTimeoutResponse extends BaseResponse {
+
+    public int ProviderPinTimeoutResponse;
+
+}
---
+++ YourPractice/src/main/java/com/remedywebsolutions/YourPractice/MedSecureAPI/requests/GetProviderPinTimeoutRequest.java
@@ -0,0 +1,45 @@
+package com.remedywebsolutions.YourPractice.MedSecureAPI.requests;
+
+import android.content.Context;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.octo.android.robospice.request.SpiceRequest;
+import com.remedywebsolutions.YourPractice.MedSecureAPI.LoggedInDataStorage;
+import com.remedywebsolutions.YourPractice.MedSecureAPI.MedSecureConnection;
+import com.remedywebsolutions.YourPractice.MedSecureAPI.POJOs.GetProviderCallsResponse;
+import com.remedywebsolutions.YourPractice.MedSecureAPI.POJOs.GetProviderPinTimeoutResponse;
+
+import java.net.HttpURLConnection;
+import java.util.HashMap;
+
+public class GetProviderPinTimeoutRequest extends SpiceRequest<GetProviderPinTimeoutResponse> {
+    private Context context;
+
+    public GetProviderPinTimeoutRequest(Context context) {
+        super(GetProviderPinTimeoutResponse.class);
+        this.context = context;
+    }
+
+    @Override
+    public GetProviderPinTimeoutResponse loadDataFromNetwork() throws Exception {
+        LoggedInDataStorage storage = new LoggedInDataStorage(context);
+        HashMap<String, String> userData = storage.RetrieveData();
+        String physicianID = userData.get("physicianID");
+        String practiceID = userData.get("practiceID");
+
+        MedSecureConnection msc = new MedSecureConnection(context);
+        msc.buildBaseURI("Communication", "GetCallsByProvider", "GET");
+
+        msc.addParameter("PhysicianID", physicianID);
+        msc.addParameter("PracticeID", practiceID);
+        HttpURLConnection connection = msc.initConnection(true);
+        String response = MedSecureConnection.getStringResult(connection);
+        connection.disconnect();
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        GetProviderPinTimeoutResponse res = mapper.readValue(response, GetProviderPinTimeoutResponse.class);
+
+        return res;
+    }
+}
